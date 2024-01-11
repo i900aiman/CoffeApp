05c58e2f3fe2b9a080c699dd5e4de6ac3e96eedc
Muhammad Aiman <76811510+i900aiman@users.noreply.github.com>
2024-01-11 09:29:28 +0800

    update file

---
+++ lib/pages/Homepage.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:coffeeuiapp/util/coffee_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.menu),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 25.0),
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favourite'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: 'Notifications')
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              "Find the best coffee for you",
+              style: GoogleFonts.bebasNeue(
+                fontSize: 56,
+              ),
+            ),
+          ),
+
+          //Spacing
+          SizedBox(
+            height: 25,
+          ),
+
+          //Search Bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: "Find your coffee",
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade600)),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade600))),
+            ),
+          ),
+
+          //Spacing
+          SizedBox(
+            height: 25,
+          ),
+
+          Expanded(
+              child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [CoffeeTile()],
+          ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/util/coffee_tile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CoffeeTile extends StatelessWidget {
+  const CoffeeTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        width: 200,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12), color: Colors.black54),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // coffee image
+            ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset('lib/images/americano.png')),
+
+            // coffee name
+            Column(
+              children: [
+                Text(
+                  "Americano",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  "With kaw kaw",
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
